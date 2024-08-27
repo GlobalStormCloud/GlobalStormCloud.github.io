58b4e3739e24e8feada60536032f905adf1df059
FliesWithWind <7544133+FliesWithWind@users.noreply.github.com>
2024-08-27 20:35:07 +0200

    New website (#12)
    * Initial new page commit
    
    * Add contact form support
    
    * Honeypot field
    
    * Footer update and cookies
    
    * Added slack notification support
    
    * Cleanup and fixes
    
    * Fix?

--- assets/js/cookieconsent-config.js
+++ assets/js/cookieconsent-config.js
@@ -1,4 +1,4 @@
-import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v3.0.0-rc.17/dist/cookieconsent.umd.js';
+// import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v3.0.0-rc.17/dist/cookieconsent.umd.js';
 
 /**
  * All config. options available here:
